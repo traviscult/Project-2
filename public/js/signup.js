24a567071ffdf5beef136b86d90904a00cd01207
traviscult <traviscult@gmail.com>
2020-05-19 16:43:05 -0400

    updated code to es6

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   // Getting references to our form and input
-  var signUpForm = $("form.signup");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
+  const signUpForm = $("form.signup");
+  const emailInput = $("input#email-input");
+  const passwordInput = $("input#password-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
-  signUpForm.on("submit", function(event) {
+  signUpForm.on("submit", (event) => {
     event.preventDefault();
-    var userData = {
+    const userData = {
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
@@ -23,7 +23,7 @@ $(document).ready(function() {
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  const signUpUser = (email, password) => {
     $.post("/api/signup", {
       email: email,
       password: password
@@ -35,7 +35,7 @@ $(document).ready(function() {
       .catch(handleLoginErr);
   }
 
-  function handleLoginErr(err) {
+  const handleLoginErr = (err) => {
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
   }
