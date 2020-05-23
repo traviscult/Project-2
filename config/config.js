eade806b7c47ba2916cbd9f26e3755a9216ba691
JidMenosta <jidmenosta@gmail.com>
2020-05-23 11:49:33 -0400

    added JAWSDB

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "jrbmf1rjnumqqbix",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: "root",
+    password: process.env.DB_PASS,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  }
+}
