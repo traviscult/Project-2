862f6cf5e20b484e164d2524771b46d2c872e183
Eddie Saunders <saunders.eddie@outlook.com>
2020-06-01 19:07:14 -0400

    quick commit to pull latest data

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -33,7 +33,6 @@ $(document).ready(() => {
 let modal = document.getElementById("myModal");
 let btn = document.getElementById("myBtn");
 let span = document.getElementsByClassName("close")[0];
-// EXS 29th May 2020 - Removed these 2 clicks as they're not terribly important to the actual login/signup process
 
 btn.onclick = function () {
   modal.style.display = "block";
