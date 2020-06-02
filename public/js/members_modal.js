87dfd377d9218fc1de25cbe30475641d9397a5b2
Shi Sha <emailtotheowner@gmail.com>
2020-06-02 18:39:59 -0400

    Modified: scripts.js, login.html, login_new.html, members.html, styles.css | Added: members_modal.js, login_new_modal.js for modal.

---
+++ public/js/members_modal.js
@@ -0,0 +1,17 @@
+// MODAL FOR members.html PAGE ONLY! 
+// EXS 1st June 2020 - This needs to be JQuery for consistency.
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+span.onclick = function () {
+  modal.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
