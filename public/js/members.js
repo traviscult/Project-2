72935dca4b8a25c4c00c5f3936b618723b01e8a4
Eddie Saunders <saunders.eddie@outlook.com>
2020-06-08 12:19:42 -0400

    Added in GeoDB call to display local city, just as with weather, if the NWS call for RDU has issues, then it will default to DC

--- public/js/members.js
+++ public/js/members.js
@@ -1,22 +1,22 @@
 // const db = require("../../models");
-$(document).ready(function() {
+$(document).ready(function () {
 
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
     // EXS 2nd June 2020 - Updated to pull NWS data for the users current Lat/Long
     //  console.log("Calling members.js");
-    $.get("/api/user_data").then(function(data) {
+    $.get("/api/user_data").then(function (data) {
         // $(".member-name").text(data.email);
-       // console.log("Our get user_data:", data);
+        // console.log("Our get user_data:", data);
         $("#modalUserName").text(data.name);
         $("#modalUserEmail").text(data.email);
         $("#greeting").text(data.name);
         getOurWeather(data.lat, data.long);
-        // console.log("Getting User Data: ", data.lat, data.long);
+        getOurPlaceName(data.lat, data.long);
     });
 
     $(".blogBtn").click(() => {
-        $.get("/api/user_data").then(function(data) {
+        $.get("/api/user_data").then(function (data) {
             let email = data.email
             let review = $("#blogCreateField").val();
             let title = $("#blogCreateInput").val();
@@ -54,8 +54,9 @@ $(document).ready(function() {
     }
 
     function displayOurWeather(ourWeatherData) {
-        console.log ("Our Weather Data: ", ourWeatherData);
+        // console.log("Our Weather Data: ", ourWeatherData);
         const currentWeatherIcon = '<img src="' + ourWeatherData.properties.periods[0].icon + '">';
+        //  $('#currentWeather').text(" Our Place Name Goes Here");
         $('#ourWeatherIcon').html(currentWeatherIcon);
         $("#wd1").text(" " + ourWeatherData.properties.periods[0].name + " " + ourWeatherData.properties.periods[0].detailedForecast);
         $("#wd2").text(" " + ourWeatherData.properties.periods[1].name + " " + ourWeatherData.properties.periods[1].detailedForecast);
@@ -71,16 +72,34 @@ $(document).ready(function() {
         $.get(ourFirstNWSURL, (response, status) => {
             const ourLongRangeForecast = response.properties.forecast;
             $.get(ourLongRangeForecast, (response, status) => {
-               displayOurWeather(response);
-            }).fail (function() {
-                console.log ("We have a fail!");
+                displayOurWeather(response);
+            }).fail(function () {
+                //      console.log ("We have a fail!");
                 // EXS 8th June default to DC if we get a fail result
                 const ourNWSErrorURL = ("https://api.weather.gov/gridpoints/LWX/95,71/forecast");
                 $.get(ourNWSErrorURL, (response, status) => {
-                   console.log("We have an error response: ",response)
-                   displayOurWeather(response);
+                    //       console.log("We have an error response: ",response)
+                    displayOurWeather(response);
                 });
             });
         });
-    } 
+    }
+    function getOurPlaceName(lat, long) {
+        ourURL = (`https://wft-geo-db.p.rapidapi.com/v1/geo/locations/${lat}${long}/nearbyCities?radius=10`)
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": ourURL,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+                "x-rapidapi-key": "8a6cd7a64emsh3dd70caa88d8d20p193484jsn53fd404332c5"
+            }
+        }
+
+        $.ajax(settings).done(function (response) {
+            $('#currentWeather').text(" " + response.data[0].city);
+        });
+
+    }
 });
